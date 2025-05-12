239c524345a38d6d12235ddfdbe308e5b1134023
SeanAndrie <gseanandrie@gmail.com>
2025-05-12 19:07:01 +0400

    Adjusted case for INT limits when parsing map

---
+++ src/utils/render_utils.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgadinga <sgadinga@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 18:24:04 by sgadinga          #+#    #+#             */
+/*   Updated: 2025/05/12 19:00:33 by sgadinga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void	scale_and_rotate(t_point3D *point, t_config config)
+{
+	point->x *= config.scale;
+	point->y *= config.scale;
+	point->z *= config.z_scale;
+	rotate_point(point, config.rotations);
+}
+
+void	reset_config(t_mlx_data *mlx)
+{
+	mlx->config = init_config();
+	redraw(mlx);
+}
